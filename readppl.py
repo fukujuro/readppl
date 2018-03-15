840325415219d71473d54d0da462476b56f7d5cf
Grit <grit.intelligence@gmail.com>
2018-03-16 00:53:57 +0700

    Add appstats

--- readppl.py
+++ readppl.py
@@ -120,11 +120,15 @@ class ReadpplApi(remote.Service):
         http_method='GET',
         name='getTopTag')
     def getTopTag(self, request):
-        q = Topic.query()
-        q = q.filter(Topic.tags==request.title)
-        q = q.order(-Topic.vote)
-        q = q.fetch(10)
-        topics = [self._copyTopicToForm(t) for t in q]
+        key = 'top:tag:' + request.title
+        topics = memcache.get(key)
+        if not topics:
+            q = Topic.query()
+            q = q.filter(Topic.tags==request.title)
+            q = q.order(-Topic.vote)
+            q = q.fetch(10)
+            topics = [self._copyTopicToForm(t) for t in q]
+            memcache.set(key)
         n = len(topics)
         return TopicForms(topics=topics, length=n)
 
